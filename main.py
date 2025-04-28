808f9a69abd608a2aa83f8c30c277390ec618f31
caneminkius <caneminkius@gmail.com>
2025-04-28 23:14:06 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,87 +1,52 @@
-#!/usr/bin/env python
-# pylint: disable=W0613, C0116
-# type: ignore[union-attr]
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import os
 import sys
-sys.path.insert(0, './objection_engine')
-
 import logging
 
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from msg_queue import Queue
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+sys.path.insert(0, './objection_engine')
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 queueList = {}
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        'Hi! This is a bot that will transform a group of Telegram messages into Ace Attorney scenes. Just forward me the messages.'
+    )
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hi! This is a bot that will transform a group of Telegram messages into Ace Attorney scenes. Just forward me the messages')
+async def about_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        'Made with ❤ by @TLuigi003.\nSource code in https://github.com/LuisMayo/ace-attorney-telegram-bot\n\n'
+        'Do you like my work? You could thank me by buying me a [ko-fi](https://ko-fi.com/luismayo)',
+        parse_mode="Markdown"
+    )
 
-
-def about_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Made with ❤ by @TLuigi003.\nSource code in https://github.com/LuisMayo/ace-attorney-telegram-bot\n\nDo you like my work? You could thank me by buying me a [ko-fi](https://ko-fi.com/luismayo)', parse_mode="Markdown")
-
-
-def getMessage(update: Update, context: CallbackContext) -> None:
-    global updater
-    if (not update.message.chat.id in queueList or queueList[update.message.chat.id] == None):
-        queueList[update.message.chat.id] = Queue(update, queueList, updater)
+async def getMessage(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global app
+    if update.message.chat.id not in queueList or queueList[update.message.chat.id] is None:
+        queueList[update.message.chat.id] = Queue(update, queueList, app)
     queueList[update.message.chat.id].addMessage(update)
 
 def main():
-    #with open('token.txt', 'r') as tokenFile:
-    #    token = tokenFile.read().replace('\r', '').replace('\n', '')
     token = os.environ["BOT_TOKEN"]
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    global updater
-    updater = Updater(token, use_context=True)
+    global app
+    app = ApplicationBuilder().token(token).build()
 
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("about", about_command))
+    app.add_handler(MessageHandler(
+        (filters.TEXT | filters.PHOTO | filters.STICKER) & ~filters.COMMAND,
+        getMessage
+    ))
 
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("about", about_command))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler((Filters.text | Filters.photo | Filters.sticker) & ~Filters.command, getMessage))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
